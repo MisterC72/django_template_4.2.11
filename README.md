e4d76831910259e0062e44171a009fe184220c01
MisterC72 <67199992+MisterC72@users.noreply.github.com>
2024-03-26 00:49:24 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# card-tracker
+A Django app for tracking credit card balances and payments
---
+++ accounts/models.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+# custom user model
+class CustomUser(AbstractUser):
+    pass
